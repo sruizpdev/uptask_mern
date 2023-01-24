158a26fdea160bd190ba67f4730b652964ad0bb5
sergiotto <sergiotto@outlook.com>
2023-01-24 12:11:13 +0100

    hasheo de password

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const usuarioShema = mongoose.Schema(
   {
@@ -29,6 +30,14 @@ const usuarioShema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+usuarioShema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const Usuario = mongoose.model("Usuario", usuarioShema);
 export default Usuario;
