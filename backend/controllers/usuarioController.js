bf32eecaa0a65a5976097a0deff46102bf8f9cc1
sergiotto <sergiotto@outlook.com>
2023-01-24 11:56:47 +0100

    evitando usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,13 @@
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req, res) => {
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("El usuario ya existe en el BBDD");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const usuario = new Usuario(req.body);
     const usuarioAlmacenado = await usuario.save();
