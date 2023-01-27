6c90b3eb55e893b96ad22b07833a11761bb0589b
sergiotto <sergiotto@outlook.com>
2023-01-27 09:42:48 +0100

    olvidePassword

--- backend/routes/usuarioRoute.js
+++ backend/routes/usuarioRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { autenticar, registrar, confirmar } from "../controllers/usuarioController.js";
+import { autenticar, registrar, confirmar,olvidePassword } from "../controllers/usuarioController.js";
 const router = express.Router();
 
 router.post("/", registrar);
 router.post('/login', autenticar)
 router.get('/confirmar/:token', confirmar)
+router.post('/olvide-password', olvidePassword)
 
 export default router;
