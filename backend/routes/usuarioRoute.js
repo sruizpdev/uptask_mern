79fc0f2cc254eb564e819741c8d05cb94ccd7651
sergiotto <sergiotto@outlook.com>
2023-01-27 10:30:46 +0100

    nuevo password

--- backend/routes/usuarioRoute.js
+++ backend/routes/usuarioRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { autenticar, registrar, confirmar,olvidePassword } from "../controllers/usuarioController.js";
+import { autenticar, registrar, confirmar,olvidePassword,comprobarToken , nuevoPassword} from "../controllers/usuarioController.js";
 const router = express.Router();
 
 router.post("/", registrar);
 router.post('/login', autenticar)
 router.get('/confirmar/:token', confirmar)
 router.post('/olvide-password', olvidePassword)
+router.get('/olvide-password/:token', comprobarToken)
+router.post('/olvide-password/:token', nuevoPassword)
 
 export default router;
