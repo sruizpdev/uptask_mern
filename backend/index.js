0b5a6b318163a8192c59a4421c7ad892488a759c
sergiotto <sergiotto@outlook.com>
2023-01-31 11:55:30 +0100

    tareaRoute, tareaController, etd

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
-import proyectoRoutes from './routes/proyectoRoutes.js'
+import proyectoRoutes from "./routes/proyectoRoutes.js";
+
+import tareaRoutes from "./routes/tareaRoutes.js";
 
 const app = express();
 
@@ -14,6 +16,7 @@ conectarDB();
 
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
+app.use("/api/tarea", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
